d6c64dcf838e215e84f1a0134f7b55e46be7ad7d
SaifAbuR3d <saefraed2@gmail.com>
2024-03-06 08:58:52 +0200

    Add DbContext and migrate to database

---
+++ EventManagement.Infrastructure/Identity/IdentityConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using EventManagement.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventManagement.Infrastructure.Identity;
+
+public static class IdentityConfiguration
+{
+    public static IServiceCollection AddIdentityInfrastructure(
+               this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddIdentity<ApplicationUser, IdentityRole<int>>(options =>
+            {
+                // TODO: Set the password requirements
+                options.Password.RequiredLength = 4;
+                options.Password.RequireDigit = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.User.RequireUniqueEmail = true;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        return services; 
+    }
+}
---
+++ EventManagement.Infrastructure/Persistence/PersistenceConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventManagement.Infrastructure.Persistence;
+
+public static class PersistenceConfiguration
+{
+    public static IServiceCollection AddPersistenceInfrastructure(
+        this IServiceCollection services, IConfiguration configuration, bool isDevelopment)
+        => services
+            .AddDatabase(configuration.GetConnectionString("SqlServer"), isDevelopment);
+
+    private static IServiceCollection AddDatabase(
+    this IServiceCollection services, string connectionString, bool isDevelopment)
+    => services
+        .AddDbContext<ApplicationDbContext>(opt =>
+            opt.UseSqlServer(connectionString)
+               .EnableSensitiveDataLogging(isDevelopment)
+               .EnableDetailedErrors(isDevelopment));
+
+}
