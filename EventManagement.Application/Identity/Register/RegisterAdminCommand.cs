bfd1a959b7145c6e1c9edaf885a2311498024541
SaifAbuR3d <saefraed2@gmail.com>
2024-03-07 06:00:05 +0200

    Implement Register Commands

--- EventManagement.Application/Identity/Register/RegisterAdminCommand.cs
+++ EventManagement.Application/Identity/Register/RegisterAdminCommand.cs
@@ -1,21 +1,33 @@
-﻿using MediatR;
+﻿using EventManagement.Domain.Abstractions.Repositories;
+using EventManagement.Domain.Enums;
+using EventManagement.Domain.Models;
+using MediatR;
 
 namespace EventManagement.Application.Identity.Register;
 
 public record RegisterAdminResponse(string Message, int AdminId);
 
-public record RegisterAdminCommand(string Email, string Password,
+public record RegisterAdminCommand(string Email, string UserName, string Password,
     string FirstName, string LastName) : IRequest<RegisterAdminResponse>;
 
-public class RegisterAdminCommandHandler(IIdentityManager identityManager)
-    : IRequestHandler<RegisterAdminCommand, RegisterAdminResponse>
+public class RegisterAdminCommandHandler(IIdentityManager identityManager, 
+    IAdminRepository adminRepository, IUnitOfWork unitOfWork) : IRequestHandler<RegisterAdminCommand, RegisterAdminResponse>
+
 {
     public async Task<RegisterAdminResponse> Handle(RegisterAdminCommand request,
         CancellationToken cancellationToken)
     {
-        var admin = await identityManager.RegisterAdmin(request.Email, request.Password,
-            request.FirstName, request.LastName);
+        var userId = await identityManager.RegisterUser(request.Email, request.UserName, request.Password,
+            request.FirstName, request.LastName, Role.Admin.ToString());
 
-        return new RegisterAdminResponse("Registration successful", admin.Id);
+        var admin = new Admin
+        {
+            UserId = userId,
+        }; 
+
+        var adminEntity = await adminRepository.AddAdminAsync(admin, cancellationToken);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return new RegisterAdminResponse("Registration successful", adminEntity.Id);
     }
 }
--- EventManagement.Application/Identity/Register/RegisterOrganizerCommand.cs
+++ EventManagement.Application/Identity/Register/RegisterOrganizerCommand.cs
@@ -1,21 +1,35 @@
-﻿using MediatR;
+﻿using EventManagement.Domain.Abstractions.Repositories;
+using EventManagement.Domain.Enums;
+using EventManagement.Domain.Models;
+using MediatR;
 
 namespace EventManagement.Application.Identity.Register;
 
 public record RegisterOrganizerResponse(string Message, int OrganizerId);
 
-public record RegisterOrganizerCommand(string Email, string Password,
+public record RegisterOrganizerCommand(string Email, string UserName, string Password,
     string FirstName, string LastName, string? CompanyName) : IRequest<RegisterOrganizerResponse>;
 
-public class RegisterOrganizerCommandHandler(IIdentityManager identityManager) 
+public class RegisterOrganizerCommandHandler(IIdentityManager identityManager, 
+    IOrganizerRepository organizerRepository, IUnitOfWork unitOfWork) 
     : IRequestHandler<RegisterOrganizerCommand, RegisterOrganizerResponse>
 {
     public async Task<RegisterOrganizerResponse> Handle(RegisterOrganizerCommand request,
         CancellationToken cancellationToken)
     {
-        var organizer = await identityManager.RegisterOrganizer(request.Email, request.Password, request.FirstName, 
-                       request.LastName, request.CompanyName);
+        var userId = await identityManager.RegisterUser(request.Email, request.UserName, 
+                       request.Password, request.FirstName, request.LastName, Role.Organizer.ToString());
 
-        return new RegisterOrganizerResponse("Registration successful", organizer.Id);
+        var organizer = new Organizer
+        {
+            UserId = userId,
+            CompanyName = request.CompanyName
+        }; 
+
+        var organizerEntity = await organizerRepository.AddOrganizerAsync(organizer, cancellationToken);
+
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return new RegisterOrganizerResponse("Registration successful", organizerEntity.Id);
     }
 }
