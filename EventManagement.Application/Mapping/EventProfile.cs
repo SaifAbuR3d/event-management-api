a4094f77202ac0458f27deb65a959e6814bbcc26
SaifAbuR3d <saefraed2@gmail.com>
2024-03-24 01:01:11 +0200

    Fix Lat,Long mapping

--- EventManagement.Application/Mapping/EventProfile.cs
+++ EventManagement.Application/Mapping/EventProfile.cs
@@ -11,6 +11,11 @@ public class EventProfile : AutoMapper.Profile
             .ForMember(dest => dest.ThumbnailUrl,
                 opt => opt.MapFrom(src => src.EventImages.First(ei => ei.IsThumbnail).ImageUrl))
             .ForMember(dest => dest.ImageUrls,
-                opt => opt.MapFrom(src => src.EventImages.Where(ei => !ei.IsThumbnail).Select(ei => ei.ImageUrl))); 
+                opt => opt.MapFrom(src => src.EventImages.Where(ei => !ei.IsThumbnail).Select(ei => ei.ImageUrl)))
+            .ForMember(dest => dest.Lat,
+                opt => opt.MapFrom(src => src.Latitude))
+            .ForMember(dest => dest.Lon,
+                opt => opt.MapFrom(src => src.Longitude)); 
+
     }
 }
