8e5060a0cb1fa7619680303f1af88442791fa485
SaifAbuR3d <saefraed2@gmail.com>
2024-04-03 07:54:07 +0200

    Add CreateBookingCommand V1 supports multiple tickets in one booking, doesn't handle/store invoices/payment/payment-method information

---
+++ EventManagement.Application/Features/Bookings/CreateBooking/CreateBookingCommandValidator.cs
@@ -0,0 +1,43 @@
+﻿using EventManagement.Application.Contracts.Requests;
+using FluentValidation;
+using static EventManagement.Domain.Constants.RequestedTicket; 
+
+namespace EventManagement.Application.Features.Bookings.CreateBooking;
+
+public class RequestedTicketValidator : AbstractValidator<RequestedTicket>
+{
+    public RequestedTicketValidator()
+    {
+        RuleFor(x => x.TicketId)
+            .NotEmpty()
+            .GreaterThan(0).WithMessage("TicketId must be greater than 0."); 
+
+        RuleFor(x => x.Quantity)
+            .NotEmpty()
+            .GreaterThan(MinQuantity).WithMessage($"Quantity must be greater than {MinQuantity}.")
+            .LessThanOrEqualTo(MaxQuantity).WithMessage($"Quantity must be less than or equal " +
+            $"to {MaxQuantity}.");
+    }
+}
+
+public class CreateBookingCommandValidator : AbstractValidator<CreateBookingCommand>
+{
+    public CreateBookingCommandValidator()
+    {
+        RuleFor(x => x.EventId)
+            .NotEmpty()
+            .GreaterThan(0).WithMessage("EventId must be greater than 0.");
+
+        RuleFor(x => x.Tickets)
+            .NotEmpty()
+            .WithMessage("Tickets must not be empty.");
+
+        RuleForEach(x => x.Tickets)
+            .SetValidator(new RequestedTicketValidator());
+
+        RuleFor(x => x.PaymentMethodId)
+            .NotEmpty()
+            .GreaterThan(0).WithMessage("PaymentMethodId must be greater than 0.");
+    }
+}
+
