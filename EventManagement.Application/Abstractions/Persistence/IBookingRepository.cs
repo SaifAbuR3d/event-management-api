cc0973f32230438f0bd65eb2640ac5eae334fc97
SaifAbuR3d <saefraed2@gmail.com>
2024-04-03 07:51:11 +0200

    Add BookingRepository, and TicketRepository to be used when making a booking

---
+++ EventManagement.Application/Abstractions/Persistence/IBookingRepository.cs
@@ -0,0 +1,15 @@
+﻿using EventManagement.Domain.Entities;
+
+namespace EventManagement.Application.Abstractions.Persistence;
+
+public interface IBookingRepository
+{
+    Task<Booking> AddBookingAsync(Booking booking, CancellationToken cancellationToken);
+    Task<Booking?> GetBookingByIdAsync(int bookingId, CancellationToken cancellationToken);
+    Task<IEnumerable<Booking>> GetBookingsByAttendeeIdAsync(int attendeeId,
+        CancellationToken cancellationToken);
+    Task<IEnumerable<Booking>> GetBookingsByEventIdAsync(int eventId,
+        CancellationToken cancellationToken);
+    Task<IEnumerable<Booking>> GetBookingsByOrganizerIdAsync(int organizerId,
+        CancellationToken cancellationToken);
+}
