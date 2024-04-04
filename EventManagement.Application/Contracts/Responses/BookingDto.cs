27fc551d2844dd4eb96d0934276e5e72211797d3
SaifAbuR3d <saefraed2@gmail.com>
2024-04-04 04:41:00 +0200

    Add BookingDto and mapper

--- EventManagement.Application/Contracts/Responses/BookingDto.cs
+++ EventManagement.Application/Contracts/Responses/BookingDto.cs
@@ -2,5 +2,8 @@
 
 public class BookingDto
 {
-
+    public int Id { get; set; }
+    public int AttendeeId { get; set; }
+    public int EventId { get; set; }
+    public ICollection<PersonTicketDto> Tickets { get; set; } = new List<PersonTicketDto>();
 }
---
+++ EventManagement.Application/Contracts/Responses/PersonTicketDto.cs
@@ -0,0 +1,16 @@
+﻿using EventManagement.Domain.Entities;
+
+namespace EventManagement.Application.Contracts.Responses;
+
+public class PersonTicketDto
+{
+    public int Id { get; set; }
+    public decimal Price { get; set; }
+    public Guid CheckInCode { get; set; }
+    public string? HolderName { get; set; }
+
+    public string TicketTypeName { get; set; } = default!;
+    public int TicketTypeId { get; set; } = default!;
+
+    public int BookingId { get; set; }
+}
