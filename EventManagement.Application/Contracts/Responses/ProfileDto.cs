b8198f1562d07b072d8961656b7406a6b1212a1f
SaifAbuR3d <saefraed2@gmail.com>
2024-04-23 20:37:17 +0300

    Fix previous, upcoming events fetching logic

--- EventManagement.Application/Contracts/Responses/ProfileDto.cs
+++ EventManagement.Application/Contracts/Responses/ProfileDto.cs
@@ -2,10 +2,10 @@
 
 public class ProfileDto
 {
-    public string? Bio { get; set; } = default!;
-    public string? Website { get; set; }
-    public string? Twitter { get; set; }
-    public string? Facebook { get; set; }
-    public string? LinkedIn { get; set; }
-    public string? Instagram { get; set; }
+    public string Bio { get; set; } = "";
+    public string Website { get; set; } = "";
+    public string Twitter { get; set; } = "";
+    public string Facebook { get; set; } = "";
+    public string LinkedIn { get; set; } = "";
+    public string Instagram { get; set; } = "";
 }
