8e5a29e67386d58f379e27f7ff81dd0aab49da62
SaifAbuR3d <saefraed2@gmail.com>
2024-05-01 21:43:37 +0300

    Fix GetIvrs query

--- EventManagement.Application/Contracts/Requests/GetAllIvrsQueryParameters.cs
+++ EventManagement.Application/Contracts/Requests/GetAllIvrsQueryParameters.cs
@@ -5,8 +5,8 @@ namespace EventManagement.Application.Contracts.Requests;
 public class GetAllIvrsQueryParameters : GetAllQueryParameters
 {
     public int? UserId { get; set; }
-    public bool? OnlyAttendees { get; set; } = false;
-    public bool? OnlyOrganizers { get; set; } = false;
+    public bool OnlyAttendees { get; set; } = false;
+    public bool OnlyOrganizers { get; set; } = false;
     public DocumentType? DocumentType { get; set; }
     public IdentityVerificationRequestStatus? Status { get; set; }
 }
