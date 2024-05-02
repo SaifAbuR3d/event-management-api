587ecd4d5dbf6061b101918f72fec3218400deb5
SaifAbuR3d <saefraed2@gmail.com>
2024-05-02 03:01:46 +0300

    Add enpoint to View Registration Requests for an Event filtered, sorted, and paginated according to the specified optional parameters, and edit the route to include eventId

--- EventManagement.Application/Abstractions/Persistence/IRegRequestRepository.cs
+++ EventManagement.Application/Abstractions/Persistence/IRegRequestRepository.cs
@@ -1,4 +1,6 @@
-﻿using EventManagement.Domain.Entities;
+﻿using EventManagement.Application.Contracts.Requests;
+using EventManagement.Application.Contracts.Responses;
+using EventManagement.Domain.Entities;
 
 namespace EventManagement.Application.Abstractions.Persistence;
 
@@ -6,5 +8,6 @@ public interface IRegRequestRepository
 {
     Task<RegistrationRequest> AddAsync(RegistrationRequest registrationRequest,
         CancellationToken cancellationToken);
+    Task<(IEnumerable<RegistrationRequest>, PaginationMetadata)> GetAllAsync(int eventId, GetAllRegRequestQueryParameters queryParameters, CancellationToken cancellationToken);
     Task<RegistrationRequest?> GetByIdAsync(int id, CancellationToken cancellationToken);
 }
---
+++ EventManagement.Application/Features/RegistrationRequests/GetAllRegRequests/GetAllRegRequestsQuery.cs
@@ -0,0 +1,70 @@
+﻿using EventManagement.Application.Abstractions.Persistence;
+using EventManagement.Application.Contracts.Requests;
+using EventManagement.Application.Contracts.Responses;
+using EventManagement.Application.Exceptions;
+using EventManagement.Application.Features.Identity;
+using EventManagement.Domain.Entities;
+using MediatR;
+
+namespace EventManagement.Application.Features.RegistrationRequests.GetAllRegRequests;
+
+public record GetAllRegRequestsQuery(int EventId, GetAllRegRequestQueryParameters Parameters)
+    : IRequest<(IEnumerable<RegRequestDto>, PaginationMetadata)>;
+
+public class GetAllRegRequestsQueryHandler(ICurrentUser currentUser,
+    IEventRepository eventRepository,
+    IRegRequestRepository repository,
+    IUserRepository userRepository)
+    : IRequestHandler<GetAllRegRequestsQuery, (IEnumerable<RegRequestDto>, PaginationMetadata)>
+{
+    public async Task<(IEnumerable<RegRequestDto>, PaginationMetadata)> Handle(
+               GetAllRegRequestsQuery request, CancellationToken cancellationToken)
+    {
+        await AuthorizeCurrentUserAccess(request, cancellationToken);
+
+        var (regRequests, paginationMetadata) = await repository.GetAllAsync(request.EventId,
+            request.Parameters, cancellationToken);
+
+        List<RegRequestDto> regRequestDtos = [];
+
+        foreach (var regRequest in regRequests)
+        {
+            var regRequestDto = new RegRequestDto
+            {
+                Id = regRequest.Id,
+                AttendeeId = regRequest.AttendeeId,
+                AttendeeUserName = await userRepository.GetUserNameByUserId(regRequest.Attendee.UserId,
+                   cancellationToken) ?? throw new CustomException("Invalid State: User Has No UserName"),
+                AttendeeProfilePictureUrl = await userRepository.GetProfilePictureByUserId(
+                    regRequest.Attendee.UserId, cancellationToken),
+                Status = regRequest.Status,
+                EventId = regRequest.EventId,
+                CreationDate = regRequest.CreationDate,
+                LastModified = regRequest.LastModified
+            };
+
+            regRequestDtos.Add(regRequestDto);
+        }
+
+        return (regRequestDtos, paginationMetadata);
+    }
+
+    private async Task AuthorizeCurrentUserAccess(GetAllRegRequestsQuery request, CancellationToken cancellationToken)
+    {
+        if (!currentUser.IsOrganizer)
+        {
+            throw new UnauthorizedException("Only organizers can view registration requests");
+        }
+
+        var @event = await eventRepository.GetEventByIdAsync(request.EventId, cancellationToken)
+            ?? throw new NotFoundException(nameof(Event), request.EventId);
+
+        var requestedEventOrganizerId = int.Parse(await userRepository.GetIdByUserId(currentUser.UserId,
+            cancellationToken) ?? throw new CustomException("Invalid State: User Has No Id"));
+
+        if (@event.OrganizerId != requestedEventOrganizerId)
+        {
+            throw new UnauthorizedException("You are not authorized to view registration requests for this event");
+        }
+    }
+}
